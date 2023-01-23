2c650a236876f892cb2cd89705fdf1d15f007ffd
Algalyq <algalyq@mail.ru>
2023-01-24 01:26:11 +0600

    Clean Arch basic

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,30 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/viper"
 	"github.com/Algalyq/Go_project"
 	"github.com/Algalyq/Go_project/pkg/handler"
+	"github.com/Algalyq/Go_project/pkg/repository"
+	"github.com/Algalyq/Go_project/pkg/service"
 )
 
 func main() {
-	handler := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatal(err.Error())
+    }
+	repo := repository.NewRepository()
+	services := service.NewService(repo)
+	handler := handler.NewHandler(services)
 
 	srv := new(goproject.Server)
-	if err := srv.Run("8080", handler.InitRoutes()); err != nil{
+	if err := srv.Run(viper.GetString("8080"), handler.InitRoutes()); err != nil{
 		log.Fatalf(err.Error())
 	}
 
+}
+
+func initConfig()  error {
+	viper.AddConfigPath("../configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,8 +1,15 @@
 package service
 
-
+import ("github.com/Algalyq/Go_project/pkg/repository")
 
 type Authorization interface {
 
 }
 
+type Service struct {
+	Authorization
+}
+
+func NewService(repos *repository.Repository) *Service {
+    return &Service{}  
+}
