44bfb497b0d0ca0f7d610923e051798d754a6d8b
algalyq <algalyq@mail.ru>
2023-04-18 00:42:06 +0600

    Adding changes and docker

--- pkg/handler/search.go
+++ pkg/handler/search.go
@@ -1,10 +1,10 @@
 package handler
 
 import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
-	"log"
+	// "bytes"
+	// "encoding/json"
+	// "io/ioutil"
+	// "log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -14,25 +14,25 @@ import (
 func (h *Handler) Search(c *gin.Context){
 	s, err := h.services.Searhing.GetSearchingProduct(c)
 
-	reqBody, errreq := json.Marshal(map[string]string{
-		"username":"admin",
-		"password":"123",
-	})
-	if errreq != nil{
-		log.Fatalln(errreq)
-	}
+	// reqBody, errreq := json.Marshal(map[string]string{
+	// 	"username":"admin",
+	// 	"password":"123",
+	// })
+	// if errreq != nil{
+	// 	log.Fatalln(errreq)
+	// }
 
 
-	resp, err2 := http.Post("http://localhost:8000/products/seller/login","application/json",bytes.NewBuffer(reqBody))
-	if err2 != nil{
-		log.Fatal(err2)
-	}
+	// resp, err2 := http.Post("http://localhost:8000/products/seller/login","application/json",bytes.NewBuffer(reqBody))
+	// if err2 != nil{
+	// 	log.Fatal(err2)
+	// }
 
-	defer resp.Body.Close()
-	body, err3 :=ioutil.ReadAll(resp.Body)
-	if err3 != nil{
-		log.Fatalln(err3)
-	}
+	// defer resp.Body.Close()
+	// body, err3 :=ioutil.ReadAll(resp.Body)
+	// if err3 != nil{
+	// 	log.Fatalln(err3)
+	// }
 
 	if err!= nil {
 		newErrorResponse(c,http.StatusBadRequest, err.Error())
@@ -40,6 +40,6 @@ func (h *Handler) Search(c *gin.Context){
 	}
 	c.JSON(http.StatusOK,map[string]interface{}{
 		"data":s,
-		"res":body,
+		// "res":body,
 	})
 }
--- pkg/repository/search_postgres.go
+++ pkg/repository/search_postgres.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"fmt"
-
 	"github.com/Algalyq/Go_project"
 	"github.com/jmoiron/sqlx"
 	"gorm.io/driver/postgres"
@@ -22,7 +21,7 @@ func NewSearchPostgres(dba *sqlx.DB) *SearchPostgres {
 
 
 func (a *SearchPostgres) GetSearchingProduct(c *gin.Context) ([]goproject.Products, error) {
-	dsn := "host=localhost user=docker password=docker dbname=test_db port=7558 sslmode=disable"
+	dsn := "host=postgres-db user=docker password=docker dbname=test_db port=5432 sslmode=disable"
 	
 	db,err := gorm.Open(postgres.Open(dsn),&gorm.Config{})
 	var products []goproject.Products
@@ -33,7 +32,7 @@ func (a *SearchPostgres) GetSearchingProduct(c *gin.Context) ([]goproject.Produc
 	sql := "SELECT fp.id,fp.producttitle,fp.price,fp.quantity,fp.pddesc, pr.image FROM furni_products AS fp join furni_productimages AS pr on fp.id = pr.product_id"
 		
 		if s := c.Query("s"); s != "" {
-			sql = fmt.Sprintf("%s WHERE fp.producttitle LIKE '%%%s%%'", sql, s)
+			sql = fmt.Sprintf("%s WHERE fp.producttitle LIKE '%%%s%%' LIMIT 1", sql, s)
 		}
 		db.Raw(sql).Scan(&products)
 
